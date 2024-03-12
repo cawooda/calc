37f7e1c6e6a905a8c3127517d54963c3dac253df
Andrew Cawood <cawooda@gmail.com>
2024-03-12 21:40:43 +1100

    object

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,7 @@
 
-
-
-
-const display = {
-    display: $('#display'),
-    result: 0,
-    change: function(key){
-        console.log(isNaN(parseInt(key));
-        if (isNaN(parseInt(key))) {
-        switch (key) {
-            case "-": this.subtractNumber();
-            case "+": this.addNumber();
-            case "/": this.divideNumber();
-            case "*": this.multiplyNumber();           
-            }
-        } else {
-            const number = parseInt(key);
-
-        }
-    },
-    addNumber: function(number){
-        this.result = this.result + number;
-        display.textContent(this.result);
-    },
-    subtractNumber: function(number){
-        this.result = this.result - number;
-        display.textContent(this.result);
-    },
-    multiplyNumber: function(number){
-        this.result = this.result * number;
-        display.textContent(this.result);
-    },
-    divideNumber: function(number){
-        this.result = this.result / number;
-        display.textContent(this.result);
-    }
-
-    
-}
-
-$display.text('0');
-
+const $display = $('#display');
 const $keyboard = $('#keyboard');
+$display.text('0');
 
 const keys = [1,2,3,4,5,6,7,8,9,"+",0,"-",'C','X','/','='];
 
@@ -56,9 +16,59 @@ for (key of keys) {
 $keyboard.on('click','.key',function(e){
     console.log(e);
     console.log(e.target);
-    display.change(e.event.data('key'));
+    display.key($(e.target).data('key'));
     
     $display.text(number + $(e.event).data('key'));
     
     
 });
+
+
+
+const display = {
+    result: "0",
+    display: function(number) {
+        $display.textContent(number);
+    },
+    key: function(key){
+        console.log(isNaN(parseInt(key)));
+        if (isNaN(parseInt(key))) {
+        switch (key) {
+            case "-": this.subtractNumber();
+            case "+": this.addNumber();
+            case "/": this.divideNumber();
+            case "*": this.multiplyNumber();           
+            }
+        } else {
+            const number = parseInt(key);
+            if (this.result == 0) {
+                this.result = number;
+                $display.textContent(this.result);
+            } else {
+                $display.textContent(this.result);
+            }
+
+        }
+    },
+    addNumber: function(number){
+        this.result = this.result + number;
+        $display.textContent(this.result);
+    },
+    subtractNumber: function(number){
+        this.result = this.result - number;
+        $display.textContent(this.result);
+    },
+    multiplyNumber: function(number){
+        this.result = this.result * number;
+        $display.textContent(this.result);
+    },
+    divideNumber: function(number){
+        this.result = this.result / number;
+        $display.textContent(this.result);
+    }
+
+    
+}
+
+
+
